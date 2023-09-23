02261a264c926dc977abe853ae4df04dc6304ea7
Shaf8808 <shadmankhan08@gmail.com>
2023-09-23 09:34:37 +0000

    Started project and created Profiles app

---
+++ profiles/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=255, blank=True)
+    content = models.TextField(blank=True)
+    image = models.ImageField(
+        upload_to='images/', default='../default_profile_y0rghg'
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.owner}'s profile"
+
+
+# Function that runs in the post_save.connect method    
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(owner=instance)
+
+
+# (function, user is the model that sends signal)
+post_save.connect(create_profile, sender=User) 
