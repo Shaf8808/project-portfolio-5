30a3b3bcc528f68173c4ad88478129e6986b2253
Shaf8808 <shadmankhan08@gmail.com>
2023-09-25 12:30:52 +0000

    Added PostDetail view to retrieve specific posts, update and delete them.

--- posts/urls.py
+++ posts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from posts import views
 
 urlpatterns = [
-    path('posts/', views.PostList.as_view())
+    path('posts/', views.PostList.as_view()),
+    path('posts/<int:pk>/', views.PostDetail.as_view()),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,10 @@
+from django.http import Http404
 from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Post
 from .serializers import PostSerializer
+from drf_api.permissions import IsOwnerOrReadOnly
 
 
 class PostList(APIView):
@@ -25,9 +27,52 @@ class PostList(APIView):
         )
         if serializer.is_valid():
             serializer.save(owner=request.user)
-            # Rerurns the data as well as the status code
+            # Returns the data as well as the status code
             return Response(
                 serializer.data, status=status.HTTP_201_CREATED
             )
         # If not valid, returns error status code 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PostDetail(APIView):
+    # Only the posts owner can edit/delete the post
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = PostSerializer
+
+    def get_object(self, pk):
+        try:
+            post = Post.objects.get(pk=pk)
+            self.check_object_permissions(self.request, post)
+            return post
+        except Post.DoesNotExist:
+            raise Http404
+    
+    # Retrieves a post by id
+    def get(self, request, pk):
+        post = self.get_object(pk)
+        serializer = PostSerializer(
+            post, context={'request': request}
+        )
+        return Response(serializer.data)
+
+    # Allows post owner to update and change a post
+    def put(self, request, pk):
+        post = self.get_object(pk)
+        serializer = PostSerializer(
+            post, data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
+        
+    # Allows post owner to delete a post
+    def delete(self, request, pk):
+        post = self.get_object(pk)
+        post.delete()
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
+        )
