3b743ce9fa7fea6e2f71c399e6a112d17bc8f0bb
Shaf8808 <shadmankhan08@gmail.com>
2023-10-01 10:47:09 +0000

    Create get_like_id method as well as include queryset fields for counting comments and likes

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Post
+from likes.models import Like
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -7,6 +8,9 @@ class PostSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    like_id = serializers.SerializerMethodField()
+    comments_count = serializers.ReadOnlyField()
+    likes_count = serializers.ReadOnlyField()
 
     def validate_image(self, value):
         if value.size > 1024 * 1024 * 2:
@@ -30,6 +34,19 @@ class PostSerializer(serializers.ModelSerializer):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            # First checks if the user already liked
+            # the post we are trying to retrieve
+            liked = Like.objects.filter(
+                owner=user, post=obj
+            ).first()
+            # Displays the like id of the user that liked
+            # a specific post
+            return liked.id if liked else None
+        return None
+
     class Meta:
         model = Post
         fields = [
@@ -37,5 +54,6 @@ class PostSerializer(serializers.ModelSerializer):
             # by django, even if it's not there in models.py
             'id', 'owner', 'created_at', 'updated_at', 'title',
             'content', 'image', 'is_owner', 'profile_id', 
-            'profile_image', 'image_filter'
+            'profile_image', 'image_filter', 'like_id',
+            'comments_count', 'likes_count'
         ]
