9e4de3366621ada2f4c60c9c05cb5f1aad09692c
Shaf8808 <shadmankhan08@gmail.com>
2023-10-25 09:40:47 +0000

    Add proxy and comment out baseURL setting in axiosDefault

--- frontend/src/api/axiosDefaults.js
+++ frontend/src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://django-rest-api-de0173352397.herokuapp.com/";
+// axios.defaults.baseURL = "https://django-rest-api-de0173352397.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
