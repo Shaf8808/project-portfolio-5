4d5fc8b5532dce4f87e84837aa4016def5dcd44a
Shaf8808 <shadmankhan08@gmail.com>
2023-11-03 16:25:20 +0000

    Add gaming field along with null and blank attributes to allow users to comment on both regular posts and gaming posts

--- comments/models.py
+++ comments/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 from posts.models import Post
+from gaming.models import Gaming
 
 
 class Comment(models.Model):
     """
-    Comment model, related to User and Post
+    Comment model, related to User and Post and Gaming
     """
-    # Two foreign keys which are related to User and Post model
+    # Three foreign keys which are related to User, Post and Gaming model
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, null=True, blank=True)
+    gaming = models.ForeignKey(Gaming, on_delete=models.CASCADE, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     content = models.TextField()
