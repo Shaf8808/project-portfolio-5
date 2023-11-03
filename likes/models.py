3d2d82ef2ba7098850d8877fd4f763736fc0f815
Shaf8808 <shadmankhan08@gmail.com>
2023-11-03 16:27:19 +0000

    Add gaming field along with null and blank attributes to allow users to like both regular posts and gaming posts. Add an appropriate related name field and adjust unique_together field

--- likes/models.py
+++ likes/models.py
@@ -1,23 +1,29 @@
 from django.db import models
 from django.contrib.auth.models import User
 from posts.models import Post
+from gaming.models import Gaming
 
 
 class Like(models.Model):
     """
-    Like model, related to 'owner' and 'post'.
+    Like model, related to 'owner', 'post' and 'gaming'.
     'owner' is a User instance and 'post' is a Post instance.
     'unique_together' makes sure a user can't like the same post twice.
     """
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(
-        Post, related_name='likes', on_delete=models.CASCADE
+        Post, related_name='likes', on_delete=models.CASCADE, null=True,
+        blank=True
+    )
+    gaming = models.ForeignKey(
+        Gaming, related_name='gaming_likes', on_delete=models.CASCADE, null=True,
+        blank=True
     )
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ['-created_at']
-        unique_together = ['owner', 'post']
+        unique_together = [['owner', 'post'], ['owner', 'gaming']]
 
     def __str__(self):
         return f'{self.owner} {self.post}'
